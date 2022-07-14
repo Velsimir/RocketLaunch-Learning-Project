13f389685c60ddaa197dadfdcabdb1b194ad8cd2
Ivan Koniashov <87714602+Velsimir@users.noreply.github.com>
2022-07-15 00:22:52 +0300

    Added sound and platforms
    Added new platforms.
    Added sound.
    Refactoring behavior of rocket

--- Rocket Launch/Assets/Scripts/RocketBehavior.cs
+++ Rocket Launch/Assets/Scripts/RocketBehavior.cs
@@ -5,31 +5,46 @@ using UnityEngine;
 public class RocketBehavior : MonoBehaviour
 {
     Rigidbody _rb;
+    AudioSource _as;
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
+        _as = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        CheckInput();
+        RocketLaunch();
+        RocketRotation();
     }
 
-    void CheckInput()
+    void RocketLaunch()
     {
         if (Input.GetKey(KeyCode.Space))
         {
             _rb.AddRelativeForce(Vector3.up);
+            if (!_as.isPlaying)
+                _as.Play();
         }
+        else
+        {
+            _as.Pause();
+        }
+    }
+
+    void RocketRotation()
+    {
+        _rb.freezeRotation = true;
         if (Input.GetKey(KeyCode.A))
         {
-            _rb.AddRelativeForce(Vector3.left);
+            transform.Rotate(Vector3.forward);
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D)) 
         {
-            _rb.AddRelativeForce(Vector3.right);
+            transform.Rotate(Vector3.back);
         }
+        _rb.freezeRotation = false;
     }
 }
